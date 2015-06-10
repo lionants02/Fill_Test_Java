cea495a2cab33eeabdf1be80dc4d209545136976
Administrator <you@example.com>
2015-06-10 13:26:00 +0700

    Add province

---
+++ Fill_Tool/src/fill_tool/C_my_Giveprivince.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fill_tool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Administrator
+ */
+public class C_my_Giveprivince {
+    Map<String, String> province=new HashMap<String, String>();
+    Map<String, String> amper=new HashMap<String, String>();
+
+    public C_my_Giveprivince() {
+        province.put("10", "กรุงเทพมหานคร");
+        province.put("11", "สมุทรปราการ");
+        province.put("12", "นนทบุรี");
+        province.put("13", "ปทุมธานี");
+        province.put("14", "พระนครศรีอยุธยา");
+        province.put("15", "อ่างทอง");
+        province.put("16", "ลพบุรี");
+        province.put("17", "สิงห์บุรี");
+        province.put("18", "ชัยนาท");
+        province.put("19", "สระบุรี");
+        province.put("20", "ชลบุรี");
+        province.put("21", "ระยอง");
+        province.put("22", "จันทบุรี");
+        province.put("23", "ตราด");
+        province.put("24", "ฉะเชิงเทรา");
+        province.put("25", "ปราจีนบุรี");
+        province.put("26", "นครนายก");
+        province.put("27", "สระแก้ว");
+        province.put("30", "นครราชสีมา");
+        province.put("31", "บุรีรัมย์");
+        province.put("32", "สุรินทร์");
+        province.put("33", "ศรีสะเกษ");
+        province.put("34", "อุบลราชธานี");
+        province.put("35", "ยโสธร");
+        province.put("36", "ชัยภูมิ");
+        province.put("37", "อำนาจเจริญ");
+        province.put("38", "บึงกาฬ");
+        province.put("39", "หนองบัวลำภู");
+        province.put("40", "ขอนแก่น");
+        province.put("41", "อุดรธานี");
+        province.put("42", "เลย");
+        province.put("43", "หนองคาย");
+        province.put("44", "มหาสารคาม");
+        province.put("45", "ร้อยเอ็ด");
+        province.put("46", "กาฬสินธุ์");
+        province.put("47", "สกลนคร");
+        province.put("48", "นครพนม");
+        province.put("49", "มุกดาหาร");
+        province.put("50", "เชียงใหม่");
+        province.put("51", "ลำพูน");
+        province.put("52", "ลำปาง");
+        province.put("53", "อุตรดิตถ์");
+        province.put("54", "แพร่");
+        province.put("55", "น่าน");
+        province.put("56", "พะเยา");
+        province.put("57", "เชียงราย");
+        province.put("58", "แม่ฮ่องสอน");
+        province.put("60", "นครสวรรค์");
+        province.put("61", "อุทัยธานี");
+        province.put("62", "กำแพงเพชร");
+        province.put("63", "ตาก");
+        province.put("64", "สุโขทัย");
+        province.put("65", "พิษณุโลก");
+        province.put("66", "พิจิตร");
+        province.put("67", "เพชรบูรณ์");
+        province.put("70", "ราชบุรี");
+        province.put("71", "กาญจนบุรี");
+        province.put("72", "สุพรรณบุรี");
+        province.put("73", "นครปฐม");
+        province.put("74", "สมุทรสาคร");
+        province.put("75", "สมุทรสงคราม");
+        province.put("76", "เพชรบุรี");
+        province.put("77", "ประจวบคีรีขันธ์");
+        province.put("80", "นครศรีธรรมราช");
+        province.put("81", "กระบี่");
+        province.put("82", "พังงา");
+        province.put("83", "ภูเก็ต");
+        province.put("84", "สุราษฎร์ธานี");
+        province.put("85", "ระนอง");
+        province.put("86", "ชุมพร");
+        province.put("90", "สงขลา");
+        province.put("91", "สตูล");
+        province.put("92", "ตรัง");
+        province.put("93", "พัทลุง");
+        province.put("94", "ปัตตานี");
+        province.put("95", "ยะลา");
+        province.put("96", "นราธิวาส");
+        province.put("S", "พัทยา");
+    }
+
+}
